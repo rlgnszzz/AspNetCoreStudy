361ec1236108d640a29cc7d693b9ff36be92ab41
gihun song <gihun.song@gmail.com>
2021-03-10 15:02:54 +0900

    Add project files.

---
+++ src/AspNetCoreStudy/Controllers/StudyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreStudy.Controllers
+{
+    public class StudyController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Java()
+        {
+            return View();
+        }
+
+        public IActionResult Csharp()
+        {
+            return View();
+        }
+
+        public IActionResult Cpp()
+        {
+            return View();
+        }
+    }
+}
